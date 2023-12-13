0bedcf97ee88ddd6b7c39f1b13128bddf312089e
MS-DOS Manfred <hofer9@gamil.com>
2023-12-14 00:43:20 +0100

    initial commit

---
+++ cleanup.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash 
+#
+# Script name: clean-up.sh
+# Description: Script for deleting everything not named "PKGBUILD" or "*.install".
+# GitLab: https://www.gitlab.com/dwt1/dtos-pkgbuild
+# Contributors: Derek Taylor
+
+# Set with the flags "-e", "-u","-o pipefail" cause the script to fail
+# if certain things happen, which is a good thing.  Otherwise, we can
+# get hidden bugs that are hard to discover.
+set -euo pipefail
+
+echo "$0"
+full_path=$(realpath "$0")
+dir_path=${full_path%/*}
+
+readarray -t x86_list <<< "$(find x86_64/ -type d | awk -F / '{print $2}' | sort -u)"
+
+for x in "${x86_list[@]}"
+do
+    cd "${dir_path}"/x86_64/"${x}"
+    echo "Cleaning up: ${x}"
+    if [ -f "PKGBUILD" ]; then
+        git clean -dff
+    else
+        echo "No PKGBUILD found in ${PWD}"
+    fi
+done
+
---
+++ move-pkgs.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+mv x86_64/*/*.pkg.tar.zst ~/iso/packages/
